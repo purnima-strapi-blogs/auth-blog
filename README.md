9f85da59676dfdce4d48e05e324e74ab3dd89261
purnimagupta <pooyax14@gmail.com>
2021-07-09 15:40:15 +0530

    strapi template for blogs

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a strapi blog template to create Strapi projects. Currently, I'm using 
+this template as backend for this [repo](https://github.com/purnima-strapi-blogs/gatsby-auth-with-strapi)
+
+
+## To run the app:
+
+```
+yarn create strapi-app my-project --template <template-github-name>
+
+Or
+
+npx create-strapi-app my-project --template <template-github-name>
+
+```
---
+++ template/api/article/controllers/article.js
@@ -0,0 +1,25 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+  
+  async findOne(ctx) {
+    const { id } = ctx.params;
+  
+    if(ctx.state.user) {
+      const entity = await strapi.services.article.findOne({ id });
+      return sanitizeEntity(entity, { model: strapi.models.article });
+    } 
+  },
+  async find (ctx) {
+    const articles = await strapi.services.article.find();
+    const entity = articles.map(({content, ...article}) => {
+      return article
+    })
+    return sanitizeEntity(entity, { model:  strapi.models.article })
+  },
+};
